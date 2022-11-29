6843bc5291e8e29a1f64fea98f1047f13f0abb13
FedericoBusero <35894905+FedericoBusero@users.noreply.github.com>
2022-11-29 20:36:57 +0100

    Update EmbAJAX.h

--- EmbAJAX.h
+++ EmbAJAX.h
@@ -353,7 +353,7 @@ protected:
 /** @brief An HTML span element with content that can be updated from the server (not the client) */
 class EmbAJAXSlider : public EmbAJAXElement {
 public:
-    EmbAJAXSlider(const char* id, int16_t min, int16_t max, int16_t initial);
+    EmbAJAXSlider(const char* id, int16_t min, int16_t max, int16_t initial, int16_t update_min_interval_ms=0);
     void print() const override;
     const char* value(uint8_t which = EmbAJAXBase::Value) const override;
     const char* valueProperty(uint8_t which = EmbAJAXBase::Value) const override;
@@ -363,7 +363,7 @@ public:
     }
     void updateFromDriverArg(const char* argname) override;
 private:
-    int16_t _min, _max, _value;
+    int16_t _min, _max, _value, _update_min_interval_ms;
 };
 
 /** @brief A color picker element (\<input type="color">) */
@@ -373,7 +373,7 @@ public:
      *  @param r Initial value for red
      *  @param g Initial value for green
      *  @param b Initial value for blue */
-    EmbAJAXColorPicker(const char* id, uint8_t r, uint8_t g, uint8_t b);
+    EmbAJAXColorPicker(const char* id, uint8_t r, uint8_t g, uint8_t b, int16_t _update_min_interval_ms=0);
     void print() const override;
     const char* value(uint8_t which = EmbAJAXBase::Value) const override;
     const char* valueProperty(uint8_t which = EmbAJAXBase::Value) const override;
@@ -384,6 +384,7 @@ public:
     void updateFromDriverArg(const char* argname) override;
 private:
     uint8_t _r, _g, _b;
+    int16_t _update_min_interval_ms;
 };
 
 /** @brief A push-button.
