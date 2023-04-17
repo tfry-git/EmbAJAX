b75de2c19686bc9c067acbccf4a6f04cd925bdd4
Thomas Friedrichsmeier <thomas.friedrichsmeier@kdemail.net>
2023-04-17 17:08:59 +0200

    Disable debug

--- EmbAJAX.h
+++ EmbAJAX.h
@@ -26,7 +26,7 @@
 #define ARDUJAX_MAX_ID_LEN 16
 
 // Set to a value above 0 for diagnostics on Serial and browser console
-#define EMBAJAX_DEBUG 3
+//#define EMBAJAX_DEBUG 3
 
 class EmbAJAXOutputDriverBase;
 class EmbAJAXElement;
